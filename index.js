4af6b21238d81fb4c1cc91a18422f81e8252f4d8
olakouns <kounassolazare@gmail.com>
2024-02-25 03:07:04 +0000

    update

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 gsap.registerPlugin(ScrollTrigger)
 
+let i = true;
+
 let tl = gsap.timeline({
     scrollTrigger: {
         trigger: ".introduction",
@@ -14,14 +16,23 @@ let tl = gsap.timeline({
 });
 
 tl.add('start')
-    .fromTo(".name-side", { x: "-50%", y: "-50%", duration: 10 }, {
-        x: "-100%", y: "-50%", duration: 10, 
-        onUpdate: () => {
-            // document.getElementsByClassName("name-side")[0].style.left = "50%"
-        },
-        onComplete: () => {
-            // document.getElementsByClassName("name-side")[0].style["text-align"] = "left"
-        }
-    }, 'start')
+    .fromTo(".name-side", { x: "-50%", y: "-50%", duration: 10,  textAlign: "center" }, {x: "-100%", y: "-50%", duration: 10, textAlign: "left"}, 'start')
     .fromTo(".image-side", { opacity: 0, x: "-50%", y: "-50%", duration: 5 }, { opacity: 1, x: "0%", y: "-50%", duration: 5 }, 'start')
     .fromTo(".name-side-action", { opacity: 0, y: 100 }, { opacity: 1, y: 0, duration: 5 });
+
+
+let tl2 = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".section-about",
+            start: "center center",
+            end: "bottom",
+            pin: true,
+            markers: true,
+            scrub: 1,
+        }
+    });
+
+tl2.add('start')
+tl2.fromTo(".section-about-h1", {opacity: 0,  y: "0"}, {opacity: 1, y: -100, duration: 10}, 'start');
+tl2.fromTo(".p-1", {opacity: 0,  x: -200}, {opacity: 1, x: 0, duration: 1000}, 'start');
+tl2.fromTo(".p-2", {opacity: 0,  x: 200}, {opacity: 1, x: 0, duration: 1000}, 'start');
